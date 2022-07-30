054ae13da3567fc317d1b447ce21279ccc4da7b5
Yuta Matsumura <tkmbmbr@gmail.com>
2022-07-30 23:46:21 +0900

    Revert "Update device page"
    This reverts commit 2ee9c2e9786254b92562b0807cf55dd9c64da378.

--- DevicePage.xaml.cs
+++ DevicePage.xaml.cs
@@ -5,36 +5,6 @@ public partial class DevicePage : ContentPage
 	public DevicePage()
 	{
 		InitializeComponent();
-        ReadDeviceInfo();
 	}
-
-    private void ReadDeviceInfo()
-    {
-        System.Text.StringBuilder sb = new System.Text.StringBuilder();
-
-        sb.AppendLine($"Model: {DeviceInfo.Current.Model}");
-        sb.AppendLine($"Manufacturer: {DeviceInfo.Current.Manufacturer}");
-        sb.AppendLine($"Name: {DeviceInfo.Name}");
-        sb.AppendLine($"OS Version: {DeviceInfo.VersionString}");
-        sb.AppendLine($"Refresh Rate: {DeviceInfo.Current}");
-        sb.AppendLine($"Idiom: {DeviceInfo.Current.Idiom}");
-        sb.AppendLine($"Platform: {DeviceInfo.Current.Platform}");
-
-        bool isVirtual = DeviceInfo.Current.DeviceType switch
-        {
-            DeviceType.Physical => false,
-            DeviceType.Virtual => true,
-            _ => false
-        };
-
-        sb.AppendLine($"Virtual device? {isVirtual}");
-
-        DisplayDeviceLabel.Text = sb.ToString();
-    }
-
-    private async void OnBackToMainClicked(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new MainPage());
-    }
 }
 
