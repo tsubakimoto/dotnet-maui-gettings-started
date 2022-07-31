c1dd5c49d542f244442a27a2973c2890718508cf
Yuta Matsumura <tkmbmbr@gmail.com>
2022-07-31 10:48:10 +0900

    デバイスページにデバイス情報を出力する

--- Pages/DevicePage.xaml.cs
+++ Pages/DevicePage.xaml.cs
@@ -6,4 +6,34 @@ public partial class DevicePage : ContentPage
     {
         InitializeComponent();
     }
+
+    protected override void OnAppearing()
+    {
+        var deviceInfoText = GetDeviceInfoText();
+        DeviceInfoLabel.Text = deviceInfoText;
+    }
+
+    private string GetDeviceInfoText()
+    {
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+        sb.AppendLine($"Model: {DeviceInfo.Current.Model}");
+        sb.AppendLine($"Manufacturer: {DeviceInfo.Current.Manufacturer}");
+        sb.AppendLine($"Name: {DeviceInfo.Name}");
+        sb.AppendLine($"OS Version: {DeviceInfo.VersionString}");
+        sb.AppendLine($"Refresh Rate: {DeviceInfo.Current}");
+        sb.AppendLine($"Idiom: {DeviceInfo.Current.Idiom}");
+        sb.AppendLine($"Platform: {DeviceInfo.Current.Platform}");
+
+        bool isVirtual = DeviceInfo.Current.DeviceType switch
+        {
+            DeviceType.Physical => false,
+            DeviceType.Virtual => true,
+            _ => false
+        };
+
+        sb.AppendLine($"Virtual device? {isVirtual}");
+
+        return sb.ToString();
+    }
 }
